3198d0f34bcabfa25604187373501661c603cc34
Chris Oliver <excid3@gmail.com>
2026-06-03 09:36:06 -0500

    Update API resources for only the supported routes

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,10 +1,10 @@
 namespace :api, defaults: {format: :json} do
   namespace :v1 do
-    resource :auth
-    resource :me, controller: :me
-    resource :password
-    resources :accounts
-    resources :users
+    resource :auth, only: [:create, :destroy]
+    resource :me, controller: :me, only: [:show, :destroy]
+    resource :password, only: [:update]
+    resources :accounts, only: [:index, :show]
+    resources :users, only: [:create]
     resources :notification_tokens, param: :token, only: [:create, :destroy]
   end
 end
--- test/controllers/api/v1/accounts_controller_test.rb
+++ test/controllers/api/v1/accounts_controller_test.rb
@@ -7,4 +7,12 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_includes response.parsed_body.pluck("name"), user.accounts.first.name
   end
+
+  test "returns current user account" do
+    user = users(:one)
+    account = accounts(:company)
+    get api_v1_account_url(account), headers: {Authorization: "token #{user.api_tokens.first.token}"}
+    assert_response :success
+    assert_includes response.parsed_body.pluck("name"), account.name
+  end
 end
