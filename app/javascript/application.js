ae5dd37edd39f39422f60d25559955e7c68ae7a1
Chris Oliver <excid3@gmail.com>
2026-07-07 11:00:37 -0500

    Use Lexxy extension for embedding iframes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "@rails/actiontext"
 import "channels"
 import "controllers"
 import "src"
-
-// Load after src so extensions work
-import { highlightCode } from "lexxy"
-import "@rails/actiontext"
-document.addEventListener("turbo:load", () => highlightCode())
-document.addEventListener("turbo:morph", () => highlightCode())
 
 import LocalTime from "local-time"
 LocalTime.start()
